47bdcc295b61f03a5ea7808dd0ada538d1662ac5
Shital Gurale <shitalgurale@gmail.com>
2026-06-15 11:26:18 +0530

    Add STATIC_ROOT and deployment files

--- backend/veterinary_ambulance/veterinary_ambulance/settings.py
+++ backend/veterinary_ambulance/veterinary_ambulance/settings.py
@@ -129,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
