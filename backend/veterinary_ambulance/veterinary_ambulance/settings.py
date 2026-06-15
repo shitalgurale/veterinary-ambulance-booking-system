00b6b9db3ff54b06aa74f1a356a5c05e611cdf9e
Shital Gurale <shitalgurale@gmail.com>
2026-06-15 10:38:19 +0530

    Update settings.py

--- backend/veterinary_ambulance/veterinary_ambulance/settings.py
+++ backend/veterinary_ambulance/veterinary_ambulance/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-va7u-u&c6v&rc8===*1f$00!r14!(#7(f+9)8k-qafj+cp(zje'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
